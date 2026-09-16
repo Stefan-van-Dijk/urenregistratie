cb02b6350d849cd4e32ec04e0cabc92ecd2ff979
Stefan-van-Dijk <stefanvandijk@me.com>
2026-09-16 10:51:25 +0200

    Ververs PWA-cache voor v0.3.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'urenregistratie-v6';
-const ASSETS = ['./','./index.html','./styles.css','./home-layout.css','./app.js','./home-layout.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE = 'urenregistratie-v7';
+const ASSETS = ['./','./index.html','./styles.css','./home-layout.css','./home-top.css','./app.js','./home-layout.js','./home-top.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));});
