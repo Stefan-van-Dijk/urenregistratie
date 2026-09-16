ffae3dad3b6cfd512271ecae3a1ebf768f3d83d4
Stefan-van-Dijk <stefanvandijk@me.com>
2026-09-16 17:32:47 +0200

    Doorbreek oude PWA-cache voor interface 0.4.3

--- home-top.js
+++ home-top.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  const APP_VERSION = '0.4.2';
+  const APP_VERSION = '0.4.3';
 
   function dateVersionText() {
     const date = new Intl.DateTimeFormat('nl-NL', {
