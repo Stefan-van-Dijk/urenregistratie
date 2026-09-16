22d33da8bedfda1649ee648bc672f5d75c4342ec
Stefan-van-Dijk <stefanvandijk@me.com>
2026-09-16 12:05:30 +0200

    Herstel bewerken en verwijderen met swipe

--- home-top.js
+++ home-top.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  const APP_VERSION = '0.3.1';
+  const APP_VERSION = '0.3.2';
 
   function dateVersionText() {
     const date = new Intl.DateTimeFormat('nl-NL', {
